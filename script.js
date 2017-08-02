d3d7944f800dfe8235f09e6587de7200dfde254c
Leah Borns <leah.borns@gmail.com>
2017-08-01 20:17:50 -0700

    javascript edits to quiz

--- script.js
+++ script.js
@@ -1,169 +1,162 @@
+/*--- Step 1 - Defining global variables ---*/
 const myQuestions = [
     {
         question: 1,
         questionstring: 'What year did french poet, Charles Piere Boudelaire coin the term modernity?',
-        answer: ['1864', '1977', '1921', '2005'],
+        glamAnswer: ['1864', '1977', '1921', '2005'],
         correct: 0
     },
 
     {
         question: 2,
         questionstring: 'As Europe modernized gender roles blurred. Men, concerned with physical appearance became known as ______.',
-        answer: ['A man about town', 'A fashionista', 'A dandy', 'A Bowie'],
+        glamAnswer: ['A man about town', 'A fashionista', 'A dandy', 'A Bowie'],
         correct: 2
     },
 
     {
         question: 3,
         questionstring: 'What two artists kicked off the Glam Rock Era in the 70s?',
-        answer: ['Elton John and Iggy Pop', 'David Bowie and Marc Bolan', 'Lou Reed and Lady Gaga', 'Suzi Quatro and The Runaways'],
+        glamAnswer: ['Elton John and Iggy Pop', 'David Bowie and Marc Bolan', 'Lou Reed and Lady Gaga', 'Suzi Quatro and The Runaways'],
         correct: 1
     },
 
     {
         question: 4,
-        questionstring: 'Who was the mentor and teacher of David Bowie?',
-        answer: ['Lindsay Kemp', 'Tiny Tim', 'Tina Turner', 'Oliver Wilde'],
+        questionstring: 'Who mentored and taught David Bowie?',
+        glamAnswer: ['Lindsay Kemp', 'Tiny Tim', 'Tina Turner', 'Oliver Wilde'],
         correct: 0
     },
 
     {
         question: 5,
         questionstring: 'What was the "excuse" for Glam Rock becoming so popular?',
-        answer: ['To make the music scene more about looks than music itself', 'It was the 70s, anything was possible', 'To honor modernization in Europe in the late 1700s', 'This time was the end of a modern era'],
+        glamAnswer: ['To make the music scene more about looks than music itself', 'It was the 70s, anything was possible', 'To honor modernization in Europe in the late 1700s', 'This time was the end of a modern era'],
         correct: 3
     },
 
     {
         question: 6,
         questionstring: 'Who was the first female bass player to become a major rock star in the 70s?',
-        answer: ['Marilyn Manson', 'Joan Jett', 'Olivia Newton John', 'Suzi Quartro'],
+        glamAnswer: ['Marilyn Manson', 'Joan Jett', 'Olivia Newton John', 'Suzi Quartro'],
         correct: 3
     },
 
     {
         question: 7,
         questionstring: 'True or False. After the release of "Dandy in the Underworld", Marc Bolan was in a car accident and killed on scene.',
-        answer: ['True', 'False'],
+        glamAnswer: ['True', 'False'],
         correct: 0
     },
 
     {
         question: 8,
         questionstring: 'What artists continue to use Glam Rock influences, today?',
-        answer: ['Andrew W.K.', 'M83', 'Lil Uzi Vert', 'BØRNS', 'All of the above'],
+        glamAnswer: ['Andrew W.K.', 'M83', 'Lil Uzi Vert', 'BØRNS', 'All of the above'],
         correct: 4
-    },
+    }
 ];
 
-const currentQuestionNumber = 0;
+let currentQuestionNumber = 0;
 const totalNumberQuestions = myQuestions.length;
-const totalCorrect = 0;
+let totalCorrect = 0;
 
-const QUESTION_ELEMENT_CLASS = ".quizQuestion";
-const QUESTION_INDEX_ATTRIBUTE = "data-item-index";
-const CURRENT_QUESTION = ".glamQuestion";
-const CURRENT_NUMBER = "span";
-const QUESTION_FORM = ".options";
-const RADIO_OPTION = "js-option";
-const RADIO_ANSWER = ".resultAnswer";
-const CORRECT_ANSWER = ".correctAnswer";
 
-function generateQuestionElement(question, questionIndex) {
+/*--- Step 2 - Defining functions ---*/
+function questionLayout() {
 
-    return `<div class="${QUESTION_ELEMENT_CLASS}" "${QUESTION_INDEX_ATTRIBUTE}" = "${questionIndex}">
-<p class="glamQuestion"><span>"${myQuestions.numberstring}"</span>"${myQuestions.questionstring}"</p>
-<form class="options" role="form">
-<input class="option" type="radio" name="${myQuestions.question}" id="${myQuestions.answer1}" value="0" required>
-<label for="${myQuestions.answer1}">"${myQuestions.answer1}"</label><br>
-<input class="option" type="radio" name="Question1" id="${myQuestions.answer2}" value="1" required>
-<label for="${myQuestions.answer2}">"${myQuestions.answer1}"</label><br>
-<input class="option" type="radio" name="Question1" id="${myQuestions.answer3}" value="2" required>
-<label for="${myQuestions.answer1}">"${myQuestions.answer3}"</label><br>
-<input class="option" type="radio" name="Question1" id="${myQuestions.answer4}" value="3" required>
-<label for="${myQuestions.answer4}">"${myQuestions.answer1}"</label>
-</form>
-<div class="submitSection">
-<!--Score and question number -->
-<button class="nextQuestion" type="submit">Next</button>
-</div>
-<div class="resultAnswer">CORRECT</div>
-</div>)`
+    //Update each question
+    $('.glamQuestion').text(myQuestions[currentQuestionNumber].questionstring);
+
+    //show multiple choices for each question
+    //Make sure each question shows appropriate choices
+    $('.choices').empty();
+
+    //get the total number of choices for the current question
+    const totalOptions = myQuestions[currentQuestionNumber].glamAnswer.length;
+
+    //loop thru all the choices for each assigned question
+    for (let i = 0; i < totalOptions; i++) {
+        var eachAnswerOption = "<input type='radio' class='option' name='option' value=" + i + "> " + myQuestions[currentQuestionNumber].glamAnswer[i] + "<br>";
+        //apply to choices div
+        $('.choices').append(eachAnswerOption);
+    }
+
+
+
+    $('.currentQuestion').text("Question " + (currentQuestionNumber + 1) + " of " + totalNumberQuestions);
+
+    $('.currentScore').text("Current Score: " + totalCorrect + " of " + totalNumberQuestions);
 }
 
-function questionHTMLstring(nextQuestion) {
-    console.log("generating next question");
-    const question = nextQuestion.map((question, questionIndex) => generateQuestionElement(question, questionIndex));
+//applying the function
 
-    return question.join("");
-}
-
-
-//Quiz questions show one at a time
-function renderQuizQuestions() {
-    console.log('`quizQuestions` ran');
-    //render question in DOM
-    const glamRockQuestion = questionHTMLstring(myQuestions);
-    //insert HTML into DOM
-    $('QUESTION_ELEMENT_CLASS').html(glamRockQuestion, function () {
-        $('RADIO_ANSWER').hide();
-        $('.startingPage').hide();
-        $('.finalpage').hide();
-
-    });
-
-
-    //.addClass(.correctAnswer) if label = myquestions.correct
-
-}
-
-//start page
-function startPage() {
-    //indiv page with button to start quiz
-    console.log('`startPage` ran');
+$(document).ready(function () {
     //starting page. Hide questions and only show starting page
     $(".quizPage").hide();
     $(".finalPage").hide();
 
-    //click start button on starting page to get to quiz
-    $(".start-quiz").click(function () {
-        $(".startingPage").hide();
-        $(".quizPage").show();
+    $('.currentScore').text("");
+    $('.currentQuestion').text("");
+
+
+    //on start page
+    $('.startButton').click(function () {
+        console.log(totalCorrect);
+        $('.finalPage').hide();
+        $('.quizPage').show();
+        $('.startingPage').hide();
+        $('resultMessage').empty();
+        questionLayout();
     });
 
+    //quiz questions
+    $('.quizPage').on('click', '.option', function () {
 
-}
+        //recognize the answer chosen
+        let chosenAnswer = $("input[class='option']:checked").val();
+        //recognize the correct answer from above
+        let correctAnswer = myQuestions[currentQuestionNumber].correct;
+        //compare each answer
+        //        console.log(chosenAnswer, correctAnswer);
+        if (chosenAnswer == correctAnswer) {
+            totalCorrect++;
+            //            console.log(totalCorrect);
+        }
+        console.log(totalCorrect);
+        //if quiz is finished show results
+        if ((currentQuestionNumber + 1) == totalNumberQuestions) {
+            //show the final score
+            $('.finalScore').text(totalCorrect + "/" + totalNumberQuestions);
 
-function basicLoopQuestion(questions) {
-    console.log(questions[0].answer[0]);
-    for (let i = 0; i <= myQuestions.length; i++); {
+            //hide other sections
+            $('.finalPage').show();
+            $('.quizPage').hide();
+            $('.startingPage').hide();
 
-    }
-}
-basicLoopQuestion(myQuestions);
-
-function printQuestions() {
-    //log question key value from 4 objects to the console
-}
-
-function getAnswer() {
-    if (answer === myQuestion[0].correct) {
-        return true;
-    }
-    getAnswer();
-
-}
-
-function handleQuizQuestions() {
-    console.log('`handleQuizQuestions` ran');
-    //prompted thru multiple choice questions
-    //one question right after the other
-    //one at a time
-    $('.resultAnswer').hide();
-    $('.submitAnswer').click(function () {
-        $('form').submit(function (event) {
-            event.preventDefault();
-
-        });
+        }
+        //if not end contiue to next question
+        else {
+            //go to the next numerical question
+            currentQuestionNumber++;
+            //show next question
+            questionLayout();
+        }
     });
-}
+
+    //click start button to reset quiz
+    $('.finalPage').on('click', '.resetQuiz', function () {
+
+        $('.finalPage').hide();
+        $('.quizPage').hide();
+        $('.startingPage').show();
+        //reset the quiz
+        currentQuestionNumber = 0;
+        totalCorrect = 0;
+
+        $('.currentScore').text("");
+        $('.currentQuestion').text("");
+        console.log(totalCorrect);
+
+    });
+});
